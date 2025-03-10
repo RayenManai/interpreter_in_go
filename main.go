12974d0e60654d866018ccd582595670609cf779
RayenManai <rayenmannai77@gmail.com>
2025-03-10 17:21:48 +0100

    evaluator done

--- main.go
+++ main.go
@@ -7,6 +7,19 @@ import (
 	"os/user"
 )
 
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
 func main() {
 	user, err := user.Current()
 	if err != nil {
@@ -14,6 +27,6 @@ func main() {
 	}
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
-	fmt.Printf(repl.MONKEY_FACE)
+	fmt.Printf(MONKEY_FACE)
 	repl.Start(os.Stdin, os.Stdout)
 }
